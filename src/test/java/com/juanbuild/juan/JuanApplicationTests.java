364d08c8d4e190594d77ab462f8b37f301c0a35c
Bin-Choi <csb5268@gmail.com>
2025-04-09 13:13:22 +0900

    init: 프로젝트 생성

---
+++ src/test/java/com/juanbuild/juan/JuanApplicationTests.java
@@ -0,0 +1,14 @@
+package com.juanbuild.juan;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
