036bfd647353485b2e9847366c7202e1b5c68428
Bin-Choi <csb5268@gmail.com>
2025-04-11 18:54:10 +0900

    test: 이름 변경 테스트 작성

---
+++ src/main/java/com/juanbuild/juan/user/dto/request/NameChangeRequestDto.java
@@ -0,0 +1,12 @@
+package com.juanbuild.juan.user.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class NameChangeRequestDto {
+    private String name;
+}
